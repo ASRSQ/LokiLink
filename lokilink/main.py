03cd75560e04298843f11a8b54bd5b752024caef
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-12-06 11:01:02 -0300

    feat: enable save to csv files

--- lokilink/main.py
+++ lokilink/main.py
@@ -3,13 +3,6 @@ import numpy as np
 import pandas as pd
 import uuid
 from collections import deque
-
-
-from entities import RandomUser, RandomNews
-from post import RandomPost
-
-# df_users = pd.DataFrame(columns=['id', 'age', 'location', 'shared_news', 'posts_made', 'days_account_exists', 'posts_per_day', 'total_posts', 'fake_news_count', 'political_news_count', 'total_posts_reach', 'total_news_shared'])
-# df_posts = pd.DataFrame(columns=['id', 'source_idx', 'destination_idx', 'news_idx', 'is_fake_news', 'is_political_news'])
 from instancers import RandomUsersInstancer, RandomNewsInstancer, RandomPostInstancer
 
 class DatasetGenerator:
@@ -20,12 +13,34 @@ class DatasetGenerator:
         self.users = []
         self.news = []
         self.posts = []
+        self.dataset_folder_path = 'data'
+
+    def generate_users_csv(self):
+        df_users = pd.DataFrame(columns=['id', 'age', 'location', 'shared_news', 'posts_made', 'days_account_exists', 'posts_per_day', 'total_posts', 'fake_news_count', 'political_news_count', 'total_posts_reach', 'total_news_shared'])
+        
+        for i, user in enumerate(self.users):
+            df_users.loc[i] = user.toJson()
+
+        os.makedirs(self.dataset_folder_path, exist_ok=True)
+        df_users.to_csv(f'{self.dataset_folder_path}/users.csv', index=False)
+
+    def generate_posts_csv(self):
+        df_posts = pd.DataFrame(columns=['id', 'source_idx', 'destination_idx', 'news_idx', 'is_fake_news', 'is_political_news'])
+
+        for i, post in enumerate(self.posts):
+            df_posts.loc[i] = post.toJson()
+
+        os.makedirs(self.dataset_folder_path, exist_ok=True)
+        df_posts.to_csv(f'{self.dataset_folder_path}/posts.csv', index=False)
+
 
 dataset = DatasetGenerator(400, 200, 4000)
-print(dataset.users)
 user_instancer = RandomUsersInstancer(dataset)
 news_instancer = RandomNewsInstancer(dataset)
 post_instancer = RandomPostInstancer(dataset)
+
+dataset.generate_users_csv()
+dataset.generate_posts_csv()
 
 print(dataset.posts)
 
@@ -71,11 +86,9 @@ print(dataset.posts)
 # for i, user in enumerate(users):
 #   df_users.loc[i] = user.toJson()
 
-# for i, post in enumerate(posts):
-#   df_posts.loc[i] = post.toJson()
+
 
 # dataset_path = './data'
 # os.makedirs(dataset_path, exist_ok=True)
 
 # df_users.to_csv(f'{dataset_path}/users.csv', index=False)
-# df_posts.to_csv(f'{dataset_path}/posts.csv', index=False)
